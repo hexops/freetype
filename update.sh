26f9ea0ce8ec934d9d53caec80f6b3d0f857892b
Stephen Gutekanst <stephen@hexops.com>
2023-06-24 18:29:43 -0700

    improve upstream merge strategy
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 git remote add upstream https://gitlab.freedesktop.org/freetype/freetype.git || true
 git fetch upstream
-git merge upstream/master --strategy ours
+git merge upstream/master || true
+git status --porcelain | awk '{if ($1=="DU") print $2}' | xargs git rm
